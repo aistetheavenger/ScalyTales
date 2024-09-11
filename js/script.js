983973926824f53453211bf517121a823f51feb7
aistetheavenger <project.blon@gmail.com>
2024-09-11 20:43:07 +0300

    change carousel to fetch loading

--- js/script.js
+++ js/script.js
@@ -23,39 +23,43 @@ fetch('menu.html')
     });
 });
 
-
-// carousel 
-document.addEventListener('DOMContentLoaded', () => {
-    const carousel = document.querySelector('.carousel');
-    const items = document.querySelectorAll('.carousel-item');
-    
-    // Duplicate the carousel items to create the infinite loop effect
-    items.forEach(item => {
-        const clone = item.cloneNode(true);
-        carousel.appendChild(clone);
-    });
-    
-    const totalWidth = carousel.scrollWidth / 2; // Width of the original items
-    
-    function setCarouselSpeed(){
-        const isMobile = window.matchMedia('(max-width: 768px').matches;
-        const speed = isMobile ? 60 : 60; //set speed for mobile and desktop
+// carousel
+fetch('carousel.html')
+.then(response => response.text())
+.then(data => {
+    document.getElementById('carousel').innerHTML = data;
+    document.addEventListener('DOMContentLoaded', () => {
+        const carousel = document.querySelector('.carousel');
+        const items = document.querySelectorAll('.carousel-item');
         
-        gsap.to(carousel, {
-            x: -totalWidth,
-            duration: speed, // Adjust speed as needed
-            ease: "linear",
-            repeat: -1,
-            overwrite: 'auto' //ensure animation is updated
+        // Duplicate the carousel items to create the infinite loop effect
+        items.forEach(item => {
+            const clone = item.cloneNode(true);
+            carousel.appendChild(clone);
         });
-    }
-    
-    setCarouselSpeed();
-    
-    window.addEventListener('resize', setCarouselSpeed);
+        
+        const totalWidth = carousel.scrollWidth / 2; // Width of the original items
+        
+        function setCarouselSpeed(){
+            const isMobile = window.matchMedia('(max-width: 768px').matches;
+            const speed = isMobile ? 60 : 60; //set speed for mobile and desktop
+            
+            gsap.to(carousel, {
+                x: -totalWidth,
+                duration: speed, // Adjust speed as needed
+                ease: "linear",
+                repeat: -1,
+                overwrite: 'auto' //ensure animation is updated
+            });
+        }
+        
+        setCarouselSpeed();
+        
+        window.addEventListener('resize', setCarouselSpeed);
+    });
 });
 
-// modal
+// modal for gallery
 function openModal(src) {
     var modal = document.getElementById("imageModal");
     var modalImg = document.getElementById("modalImage");
